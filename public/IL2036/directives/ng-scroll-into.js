6e267f03563884130c1bde573ba4fe3f38c562ca
shnizle <dizlle@gmail.com>
2015-12-17 21:39:57 +0200

    fixed initial fade in issue

--- public/IL2036/directives/ng-scroll-into.js
+++ public/IL2036/directives/ng-scroll-into.js
@@ -1,5 +1,8 @@
 function checkIfElemInScreen(elem){
 	
+	//console.log(elem.getBoundingClientRect().top + "<" + window.innerHeight);
+	//console.log($(elem).offset().top);
+	console.log(elem.getBoundingClientRect().top);	
 	return (elem.getBoundingClientRect().top + 60 < window.innerHeight);
 }
 
@@ -9,12 +12,21 @@ app.directive("ngScrollInto", ['$parse', function ($parse) {
       link: function (scope, elem, attrs) {
         var fn = $parse(attrs.ngScrollInto);
         
+        //if(checkIfElemInScreen(elem[0])){
+	          	//	$(elem).animate({opacity:1}, 300);
+          //		}
+        
         $(document).scroll( function() {
 	          
 	          if(checkIfElemInScreen(elem[0])){
 	          		$(elem).animate({opacity:1}, 300);
           		}
         });
+        
+        $('html, body').animate({
+	        scrollTop: 100
+	    }, 10);
+         
       }
     };
   }]);
